da93b04056b67cf7600ef3f30ff5447eb6fda6b9
harsh_sportsq <theunforgivensol@gmail.com>
2021-03-05 21:49:00 +0530

    chore(exercise attempt): five-three atomic-append. Did not understand the question correctly

--- chap5-file-IO-further-details/five-three/atomic-append.c
+++ chap5-file-IO-further-details/five-three/atomic-append.c
@@ -10,7 +10,9 @@
 int
 main(int argc, char *argv[])
 {
-  printf("pid: [%ld]\n", (long) getpid());
+  pid_t pid; 
+  pid = getpid();
+  printf("pid: [%ld]\n", (long) pid);
   if (argc < 3 || strcmp(argv[1], "--help") == 0)
   {
 	usageErr("Usage: %s filename num-bytes [x]\n", argv[0]);
@@ -19,31 +21,40 @@ main(int argc, char *argv[])
   int fd, openFlags;
   ssize_t numWrites;
   off_t fileOffset;
-  //pid_t pid; 
   char buf[BUF_SIZE];
 
   //openFlags = 0;
-  openFlags = O_RDWR | O_CREAT | ((*argv[argc-1]) == 'x' ? O_APPEND : 1);
-
-  /*
+  /* 
    * CAUTION!
-   * If file is opened with only perm flag S_IWUSR, then trying
-   * to access it will throw 
-   * EACCESS Permission denied] open
-   * error.
+   *
+   * Property of OR GATE:
+   * To keep input unchanged OR it with 0.
+   * ORing with 1 results in output of 1, no matter what the input.
    */
-  if ((fd = open(argv[1], openFlags, S_IRUSR | S_IWUSR)) == -1)
-	errExit("open");
-
   /*
    * CAUTION!
    *
    * `*argv[argc-1] == 'x'` is wrong because
    * *argv[argc-1] will give first character of argument string
-   * at position argc-1.
+   * at position argc-1 in array of pointers argv.
    *
-   * See strcmp()
+   * See strcmp() for comparing to strings(or character) the right way.
+   *
    */
+  openFlags = O_RDWR | O_CREAT | ((*argv[argc-1]) == 'x' ? O_APPEND : 0);
+  printf("openFlags: %d\n", openFlags);
+  printf("addtional argument value: %c\n", *argv[argc-1]);
+
+  /*
+   * CAUTION!
+   * If file is opened with only perm flag S_IWUSR, then trying
+   * to access it will throw 
+   * [EACCESS Permission denied] open
+   * error.
+   */
+  if ((fd = open(argv[1], openFlags, S_IRUSR | S_IWUSR)) == -1)
+	errExit("open");
+
   if (*argv[argc-1] == 'x')
   {
 	if ((fileOffset = lseek(fd, 0, SEEK_END)) == -1)
@@ -53,7 +64,7 @@ main(int argc, char *argv[])
 
   if ((numWrites = write(fd, buf, BUF_SIZE)) == -1)
 	errExit("write");
-  printf("Written %ld bytes\n", (long) numWrites);
+  printf("[%ld]: Written %ld bytes\n", (long) pid, (long) numWrites);
 
   if(close(fd) == -1)
 	errExit("close");
