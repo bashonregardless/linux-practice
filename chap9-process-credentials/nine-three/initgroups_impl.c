0fc2b613eda413199de9464f2d02c267318e6646
harsh_sportsq <theunforgivensol@gmail.com>
2021-03-26 17:13:34 +0530

    chore(exercise attempt): nine-three initgroups() implementation. FIXED initgroups() error, NOW complete the problem

--- chap9-process-credentials/nine-three/initgroups_impl.c
+++ chap9-process-credentials/nine-three/initgroups_impl.c
@@ -1,7 +1,10 @@
 /* This code is written to test the functionality of
  * initgroups() api.
  *
- * FIXME the code fails at initgroups() call.
+ * DONE FIXME the code fails at initgroups() call.
+ *
+ * NOTE make use of errno and other methods available
+ * to identify the cause of error.
  */
 /* TODO After the above problem is fixed, proceed with the
  * solution to problem.
@@ -16,6 +19,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <errno.h>
+#include <string.h>
+
 #define groupsize 100
 int
 main (int argc, char **argv)
@@ -23,8 +29,22 @@ main (int argc, char **argv)
   gid_t group, grouplist[groupsize];
   int gid_size;
   group = 123;
+
+  /* A privileged process can use setgroups() and initgroups() to 
+   * change its set of supplementary group IDs.
+   * 
+   * Is your process priviledged?
+   */
+  /* ERRORS:
+   *   ENOMEM Insufficient memory to allocate group information structure.
+   *   EPERM  The calling process has insufficient privilege.  See the underlying system call setgroups(2)
+   */
   if (initgroups(argv[1], group) == -1)
+  {
+	printf("%s\n", strerror(errno));
 	exit(EXIT_FAILURE);
+  }
+
   if ((gid_size = getgroups(groupsize, grouplist)) == -1)
 	exit(EXIT_FAILURE);
 
