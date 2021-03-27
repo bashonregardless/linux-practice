3da9756ca13f9486b496870517bca5ba9c3e34f6
harsh_sportsq <theunforgivensol@gmail.com>
2021-03-27 15:22:51 +0530

    chore(in text concept): irrevocably drop all privileges

--- chap9-process-credentials/inTextConcepts/irrevocablyDropAllPrivileges.c
+++ chap9-process-credentials/inTextConcepts/irrevocablyDropAllPrivileges.c
@@ -1,11 +1,27 @@
+#include <sys/types.h>
+#include <fcntl.h>
+
 #include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
 #include "tlpi_hdr.h"
 
 int
-main()
+main(int argc, char **argv)
 {
+  int fd;
   uid_t process_uid;
   printf("%d\n", getuid());
+
+  if ((fd = open(argv[1], O_RDONLY)) == -1)
+	printf("%s\n", strerror(errno));
   if (setuid(getuid()) == -1)
 	errExit("setuid");
+  printf("%d\n", getuid());
 }
+
+/*
+ * Behavior of this program under various values of IDs:
+ *
+ */
