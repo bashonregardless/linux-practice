b48207f8d98dc789550e598e719e76ca6ef5b975
harsh_sportsq <theunforgivensol@gmail.com>
2021-04-02 20:37:42 +0530

    chore(exercise attempt): twelve-one process list. TODO further refactor

--- chap12/twelve-two/p_id_cmdn_si.c
+++ chap12/twelve-two/p_id_cmdn_si.c
@@ -17,6 +17,9 @@
 #include "ugid_functions.h"
 
 #include <ctype.h>
+
+void readfdata(char *pathname, uid_t uid);
+
 void trim(char * s);
 
 void trim(char * s) {
@@ -51,22 +54,92 @@ userIdFromName(const char *name)
     return pwd->pw_uid;
 }
 
+void readfdata(char *pathname, uid_t uid)
+{
+  pid_t pid;
+  char *token, *name;
+  char *saveptr;
+  long realuid;
+  char line[128];
+  FILE *fst;
+
+  /* Open file stream */
+  if ((fst = fopen(pathname, "r")) == NULL) {
+	printf("%s \n", strerror(errno));
+	return; /* Continue if fopen fails */
+  }
+
+  pid = -1;
+  name = NULL;
+  realuid = -1;
+  /* Do some work on file, a line at a time. */
+  while (!(pid != -1 && name != NULL && realuid != -1)
+	  && fgets(line, sizeof line, fst) != NULL) {
+	/* Tokenize
+	 * 	   * delimiter ":" */
+	if ((token = strtok_r(line, ":", &saveptr)) == NULL) {
+	  printf("No token\n");
+	  continue; /* Continue if no token found */
+	}
+
+	/* If token is either "Name" or "Uid" */
+	if (strcmp(token, "Pid") == 0) {
+	  /* Get next token */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Continue if no token found */
+	  }
+	  trim(token);
+	  pid = getInt(token, GN_GT_0, "pid");
+	}
+
+	if (strcmp(token, "Name") == 0) {
+	  /* Get next token */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Continue if no token found */
+	  }
+	  name = token;
+	}
+
+	if (strcmp(token, "Uid") == 0) {
+	  /* Tokenize
+	   * delimiter: "\t" */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Continue if no token found */
+	  }
+
+	  if ((token = strtok_r(token, "\t", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Continue if no token found */
+	  }
+	  trim(token);
+	  realuid = getInt(token, GN_ANY_BASE, "realuid");
+
+	  if (uid == realuid) {
+		if (pid != -1 && name != NULL) {
+		  printf("Pid: %ld\nName: %s\n", (long) pid, name);
+		  printf("\n\n");
+		  break;
+		}
+	  }
+	}
+  }
+  /* Close file stream */
+  if (fclose(fst) == -1)
+	errExit("close");
+}
+
 int
 main (int argc, char **argv)
 {
-  int dfd;
-  pid_t pid;
   char pathname[100], dirname[256];
   struct dirent *dirst;
   char *base = "/proc/";
   DIR *procdirstream;
-  FILE *fst;
   struct stat sb;
   uid_t uid;
-  char line[128];
-  char *token, *name;
-  char *saveptr;
-  long realuid;
 
   if (argc != 2 || strcmp(argv[1], "--help") == 0)
 	usageErr("%s username\n", argv[0]);
@@ -77,10 +150,6 @@ main (int argc, char **argv)
   /* open dir stream */
   if ((procdirstream = opendir(base)) == NULL) {
 	strerror(errno);
-	exit(EXIT_FAILURE);
-  }
-
-  if ((dfd = dirfd(procdirstream)) == -1) {
 	exit(EXIT_FAILURE);
   }
 
@@ -96,7 +165,7 @@ main (int argc, char **argv)
 	if((strcmp(dirst->d_name, ".") == 0) || (strcmp(dirst->d_name, "..") == 0))
 	  continue; /* Skip . and .. */
 
-	/* Convert integer to be used */
+	/* Form pathname to be used */
 	/* TODO is the code portion to allocate size correct? */
 	snprintf(pathname, sizeof(dirname),
 		"%s%s/status", base, dirst->d_name);
@@ -106,83 +175,15 @@ main (int argc, char **argv)
 	  continue; /* Continue if stat call fails */
 	}
 
-	printf("%s\n", pathname);
+	//printf("%s\n", pathname);
 
 	if ((sb.st_mode & S_IFMT) != S_IFREG) /* Check for file type */
 	  continue; /* Continue if not a regular file */
 
 	/* Handle regular file */
 
-	/* Open file stream */
-	if ((fst = fopen(pathname, "r")) == NULL) {
-	  printf("%s \n", strerror(errno));
-	  continue; /* Continue if fopen fails */
-	}
-
 	/* Do some work on file, a line at a time. */
-
-	pid = -1;
-	name = NULL;
-	realuid = -1;
-	/* Do some work on file, a line at a time. */
-	while (!(pid != -1 && name != NULL && realuid != -1)
-		&& fgets(line, sizeof line, fst) != NULL) {
-	  /* Tokenize
-	   * delimiter ":" */
-	  if ((token = strtok_r(line, ":", &saveptr)) == NULL) {
-		printf("No token\n");
-		continue; /* Continue if no token found */
-	  }
-
-	  /* If token is either "Name" or "Uid" */
-	  if (strcmp(token, "Pid") == 0) {
-		/* Get next token */
-		if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
-		  printf("No token\n");
-		  continue; /* Continue if no token found */
-		}
-		trim(token);
-		pid = getInt(token, GN_GT_0, "pid");
-	  }
-
-	  if (strcmp(token, "Name") == 0) {
-		/* Get next token */
-		if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
-		  printf("No token\n");
-		  continue; /* Continue if no token found */
-		}
-		name = token;
-	  }
-
-	  if (strcmp(token, "Uid") == 0) {
-		/* Tokenize
-		 * delimiter: "\t" */
-		if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
-		  printf("No token\n");
-		  continue; /* Continue if no token found */
-		}
-	
-		if ((token = strtok_r(token, "\t", &saveptr)) == NULL) {
-		  printf("No token\n");
-		  continue; /* Continue if no token found */
-		}
-		trim(token);
-		realuid = getInt(token, GN_ANY_BASE, "realuid");
-
-		if (uid == realuid) {
-		  if (pid != -1 && name != NULL) {
-			printf("Pid: %ld\nName: %s\n", (long) pid, name);
-			break;
-		  }
-		}
-	  }
-	}
-
-	/* Close file stream */
-	if (fclose(fst) == -1)
-	  errExit("close");
-
-	printf("\n\n");
+	readfdata(pathname, uid);
   }
 
   /* check  any  error */
