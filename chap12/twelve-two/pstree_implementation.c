fdfc5cc2d69c2878a9cb0eeb27210536553ccb5b
harsh_sportsq <theunforgivensol@gmail.com>
2021-04-20 16:31:43 +0530

    chore(Makefile refactor and exer attempt): twelve-two

--- chap12/twelve-two/pstree_implementation.c
+++ chap12/twelve-two/pstree_implementation.c
@@ -8,14 +8,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <errno.h>
+
+#include "tlpi_hdr.h"
+
 struct node {
+  int children_count;
+  pid_t pid;
+  pid_t ppid;
   char *proc_name;
+  struct node* next;
 };
 
 int
 main(int args, char **argv)
 {
+  DIR dirst;
+  char *pathname;
 
-  pid_t ppid, pid;
+  pathname = "/proc/";
+  if ((dirst = opendir(pathname)) == NULL) {
+	/* TODO Is it true that I don't have to pass errno
+	 * explicitly to errExit().
+	 * If the above statement is true, then during dynamic
+	 * linking phase this was taken care of.
+	 */
+	errExit("opendir");
+  }
+
   exit(EXIT_SUCCESS);
 }
