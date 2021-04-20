d067dd22fb1561804ccdbbd20da8303f9511e3ce
harsh_sportsq <theunforgivensol@gmail.com>
2021-04-21 03:51:58 +0530

    chore(exer attempt): twelve-two. addnode functionality under dev

--- chap12/twelve-two/pstree_implementation.c
+++ chap12/twelve-two/pstree_implementation.c
@@ -7,6 +7,7 @@
 #include <fcntl.h>
 #include <dirent.h>
 #include <sys/stat.h>
+#include <ctype.h>
 
 #include "tlpi_hdr.h"
 
@@ -19,6 +20,95 @@ struct node {
   char *proc_name;
   struct node* next;
 };
+
+void addnode(pid_t);
+
+void readfdata(char *);
+
+void trim(char *);
+
+void trim(char * s) {
+  char * p = s;
+  int l = strlen(p);
+
+  while(isspace(p[l - 1])) p[--l] = 0;
+  while(* p && isspace(* p)) ++p, --l;
+
+  memmove(s, p, l + 1);
+}
+
+void addnode(pid_t pid) {
+  struct node *pproc_node;
+
+  if ((pproc_node = malloc(sizeof(struct node))) == NULL)
+	errExit("malloc addnode");
+}
+
+void readfdata(char *pathname)
+{
+  pid_t ppid, pid;
+  char *token;
+  char *saveptr;
+  char line[128];
+  FILE *fst;
+
+  /* Open file stream */
+  if ((fst = fopen(pathname, "r")) == NULL) {
+	printf("%s \n", strerror(errno));
+	return; /* Continue if fopen fails */
+  }
+
+  pid = -1;
+  ppid = -1;
+  /* Do some work on file, a line at a time. */
+  while (fgets(line, sizeof line, fst) != NULL) {
+	/* Tokenize
+	 * delimiter ":" */
+	if ((token = strtok_r(line, ":", &saveptr)) == NULL) {
+	  printf("No token\n");
+	  continue; /* Skip if no token found */
+	}
+
+	/* If token is Pid */
+	if (strcmp(token, "Pid") == 0) {
+	  /* Get next token */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Skip if no token found */
+	  }
+	  trim(token);
+	  pid = getInt(token, GN_ANY_BASE, "pid");
+	  printf("Pid: string %s or integer %ld\n", token, (long) pid);
+	  addnode(pid);
+	}
+
+	/* If token is PPid */
+	if (strcmp(token, "PPid") == 0) {
+	  /* Get next token */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Skip if no token found */
+	  }
+	  trim(token);
+	  ppid = getInt(token, GN_ANY_BASE, "ppid");
+	  printf("PPid: string %s or integer %ld\n", token, (long) ppid);
+	}
+
+	/* If token is "Name"*/
+	if (strcmp(token, "Name") == 0) {
+	  /* Get next token */
+	  if ((token = strtok_r(NULL, ":", &saveptr)) == NULL) {
+		printf("No token\n");
+		continue; /* Skip if no token found */
+	  }
+	  trim(token);
+	  printf("Name: %s\n", token);
+	}
+  }
+  /* Close file stream */
+  if (fclose(fst) == -1)
+	errExit("close");
+}
 
 int
 main(int args, char **argv)
@@ -51,6 +141,7 @@ main(int args, char **argv)
    * 	sys/types
    * 	string
    */
+
   base = "/proc/";
   if ((procdirst = opendir(base)) == NULL) {
 	/* TODO Is it true that I don't have to pass errno
@@ -61,26 +152,8 @@ main(int args, char **argv)
 	errExit("opendir");
   }
 
-  while (1) {
-	errno = 0;
-	/* TODO
-	 * NOTE keeping `errno = 0` initalization outside while loop
-	 * sets errno and takes path of errExit() in
-	 * readdir() error checking code below.
-	 * This is probably because, some funtion below in this loop
-	 * sets errno to non-zero value.
-	 */
-
-	/* Read from directory stream */
-	if ((dirst = readdir(procdirst)) == NULL) {
-	  if (errno == 0) {
-		printf("\n\nEnd of dir strean\n\n");
-		break;
-	  }
-	  else
-		errExit("readdir"); 
-	}
-
+  errno = 0;
+  while ((dirst = readdir(procdirst)) != NULL) { /* Read from directory stream */
 	if((strcmp(dirst->d_name, ".") == 0) || (strcmp(dirst->d_name, "..") == 0))
 	  continue; /* Skip . and .. */
 
@@ -88,7 +161,7 @@ main(int args, char **argv)
 	snprintf(proc_dirname, PATHNAME_BUF_SIZE, "%s%s", base, dirst->d_name);
 
 	if (stat(proc_dirname, &sb) == -1) {
-	  printf("Cannot open file.: stat proc_dirname: %s\n", proc_dirname);
+	  printf("stat proc_dirname: %s \tError: %s\n", proc_dirname, strerror(errno));
 	  continue;
 	}
 
@@ -99,7 +172,7 @@ main(int args, char **argv)
 	snprintf(pathname, PATHNAME_BUF_SIZE, "%s/status", proc_dirname); 
 
 	if (stat(pathname, &sb) == -1) {
-	  printf("Cannot open file.: stat file pathname: %s\n", pathname);
+	  printf("stat pathname: %s \tError: %s\n", pathname, strerror(errno));
 	  continue;
 	}
 
@@ -107,7 +180,28 @@ main(int args, char **argv)
 	  continue; /* Skip if not a regular file */
 
 	printf("%s\n", pathname);
-  }
+	
+	errno = 0; /* Reset errno because
+	NOTE errno could also be set by api such as stat() */
+	/* TODO
+	 * NOTE keeping `errno = 0` initalization outside while loop
+	 * sets errno and takes path of errExit() in
+	 * readdir() termination code below.
+	 * This is probably because, some funtion below in this loop
+	 * sets errno to non-zero value.
+	 */
+	
+	/* Do something with file */
+
+	readfdata(pathname);
+	printf("\n");
+  } 
+
+  /* Hanlde termination of readdir() */
+  if (errno == 0) /* End of dir stream reached */
+	printf("\n\nEnd of dir strean\n\n");
+  else
+	errExit("readdir"); 
 
   if (closedir(procdirst) == -1)
 	errExit("closedir");
